7f75c3da8ced21312ea075b00221ef0cd0176eb0
ShafiArnob <shafiarnob@gmail.com>
2024-01-17 10:10:55 +0600

    full pyramid

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-/* Q: Hollow Inverted Half Pyramid
- **********
- *       *
- *      *
- *     *
- *    *
- *   *
- *  *
- * *
- **
- *
+/* Q: Full Pyramid
+// prettier-ignore
+                            *
+                         *  *  *
+                      *  *  *  *  *
+                   *  *  *  *  *  *  *
+                *  *  *  *  *  *  *  *  *
+             *  *  *  *  *  *  *  *  *  *  *
+          *  *  *  *  *  *  *  *  *  *  *  *  *
+       *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+    *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+ *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *        
  */
-function hollowInvertedHalfPyramid(n) {
+function fullPyramid(n) {
   let pyramid = "";
   for (let row = 0; row < n; row++) {
-    for (let col = 0; col < n; col++) {
-      if (row == 0 || col == 0 || col == n - row - 1) {
-        pyramid += "*";
+    let k = 0;
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (col < n - row - 1) {
+        pyramid += "   ";
+      } else if (k < 2 * row + 1) {
+        pyramid += " * ";
+        k++;
       } else {
-        pyramid += " ";
+        pyramid += "   ";
       }
+      // pyramid += "*";
     }
     pyramid += "\n";
   }
   return pyramid;
 }
 
-console.log(hollowInvertedHalfPyramid(10));
+console.log(fullPyramid(10));
--- patterns/triangles.js
+++ patterns/triangles.js
@@ -49,6 +49,39 @@ function pyramid(row) {
   return pyramid;
 }
 
+/* Q: Full Pyramid
+// prettier-ignore
+                            *
+                         *  *  *
+                      *  *  *  *  *
+                   *  *  *  *  *  *  *
+                *  *  *  *  *  *  *  *  *
+             *  *  *  *  *  *  *  *  *  *  *
+          *  *  *  *  *  *  *  *  *  *  *  *  *
+       *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+    *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *
+ *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *  *        
+ */
+function fullPyramid(n) {
+  let pyramid = "";
+  for (let row = 0; row < n; row++) {
+    let k = 0;
+    for (let col = 0; col < 2 * n - 1; col++) {
+      if (col < n - row - 1) {
+        pyramid += "   ";
+      } else if (k < 2 * row + 1) {
+        pyramid += " * ";
+        k++;
+      } else {
+        pyramid += "   ";
+      }
+      // pyramid += "*";
+    }
+    pyramid += "\n";
+  }
+  return pyramid;
+}
+
 /* Q: Hollow Inverted Half Pyramid
  **********
  *       *
